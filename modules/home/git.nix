914d5fff0aaf82ad7763ea38e478a65d47913eae
IgnacyWie <ignacywie@icloud.com>
2026-06-24 15:57:03 +0200

    feat(signature): added signature to gamma

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,21 +1,31 @@
-{ ... }:
+{ config, ... }:
 
 {
+  home.file.".ssh/allowed_signers".text = ''
+    ignacywie@icloud.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfJhSA0QU8AkhkNOCrO1EavY+D3tfcANzA90apm5LOd Github Access Developer Key
+  '';
+
   programs.git = {
     enable = true;
+    lfs.enable = true;
 
     settings = {
       branch.sort = "-committerdate";
-      commit.gpgSign = false;
+      commit.gpgSign = true;
       core.editor = "nvim";
       fetch.prune = true;
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+      };
       init.defaultBranch = "main";
       pull.rebase = true;
       push.autoSetupRemote = true;
       rerere.enabled = true;
       user = {
         email = "ignacywie@icloud.com";
-        name = "Ignacy Wielogorski";
+        name = "IgnacyWie";
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfJhSA0QU8AkhkNOCrO1EavY+D3tfcANzA90apm5LOd Github Access Developer Key";
       };
     };
   };
